7f2e55f8b0bf73b576df118b722d423d6cbbe5df
Joao <joao.khalaf@gmail.com>
2025-01-28 23:57:55 -0300

    just one line change

--- components/navbar-links.tsx
+++ components/navbar-links.tsx
@@ -10,7 +10,7 @@ function NavLinks() {
             </div>
             <div className="flex gap-4 text-sm border rounded-md px-2 py-1">
               
-              <a href="/" className="hover:text-gray-600 text-sm  px-2 py-1">/HOME</a>
+              <a href="/" className="hover:text-gray-600 text-sm  px-2 py-1">/WORK</a>
               <a href="/writing" className="hover:text-gray-600 text-sm  px-2 py-1">/WRITING</a>
               <a href="/about" className="hover:text-gray-600 text-sm  px-2 py-1">/ABOUT</a>
             </div>

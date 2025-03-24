72e47b9be9bb29c66192bac42e0a755e62129510
Joao <joao.khalaf@gmail.com>
2025-03-23 22:53:58 -0300

    feat: add banner image to navbar and improve body styling

--- components/navbar-links.tsx
+++ components/navbar-links.tsx
@@ -4,22 +4,35 @@ import { ThemeToggle } from './theme-toggle'
 import LocaleSwitcher from './locale-switcher'
 import { useTranslations } from 'next-intl'
 import { Link } from '@/i18n/navigation'
+import Image from 'next/image';
 
 function NavLinks() {
   const t = useTranslations('Navigation');
   
   return (
-    <nav className="flex justify-center items-center mb-12 ">
-       <ThemeToggle />
-            <div className="invisible sm:visible">
-            </div>
-            <div className="flex gap-4 text-sm border rounded-md px-2 py-1">
-              <Link href="/" className="hover:text-gray-600 text-sm px-2 py-1">{t('work')}</Link>
-              <Link href="/writing" className="hover:text-gray-600 text-sm px-2 py-1">{t('writing')}</Link>
-              <Link href="/about" className="hover:text-gray-600 text-sm px-2 py-1">{t('about')}</Link>
-            </div>
+    <>
+    <div className="mb-4">
+      <Image 
+        src="/imgs/banner.jpg"
+        alt=''
+        width={1800}
+        height={60}
+        className='object-cover'>
+      </Image>
+    </div>
+
+    <nav className="flex justify-center items-center mb-12 w-full">
+      <div className="flex items-center gap-4">
+        <ThemeToggle />
+        <div className="flex gap-4 text-sm border rounded-md px-2 py-1">
+          <Link href="/" className="hover:text-gray-600 text-sm px-2 py-1">{t('work')}</Link>
+          <Link href="/writing" className="hover:text-gray-600 text-sm px-2 py-1">{t('writing')}</Link>
+          <Link href="/about" className="hover:text-gray-600 text-sm px-2 py-1">{t('about')}</Link>
+        </div>
+      </div>
         <LocaleSwitcher />
     </nav>
+    </>
   )
 }
 
